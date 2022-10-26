8e27cc9ae32d530067b27c643c4a56386486fea4
Hibiki -khorosho <583044063@qq.com>
2022-10-26 17:33:57 +0800

    添加api1 简单测试

---
+++ TrueCredit.Auth.Server/TestApi1/Program.cs
@@ -0,0 +1,50 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using OpenIddict.Validation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//¿çÓòÅäÖÃ
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+{
+    // ÉèÖÃÎªÔÊÐíËùÓÐ¿çÓò
+    builder.SetIsOriginAllowed(_ => true).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+}));
+// Register the OpenIddict validation components.
+builder.Services.AddOpenIddict()
+    .AddValidation(options =>
+    {
+        // Note: the validation handler uses OpenID Connect discovery
+        // to retrieve the address of the introspection endpoint.
+        options.SetIssuer("https://localhost:44330/");
+        options.AddAudiences("resource_server_1");
+
+        // Configure the validation handler to use introspection and register the client
+        // credentials used when communicating with the remote introspection endpoint.
+        options.UseIntrospection()
+               .SetClientId("resource_server_1")
+               .SetClientSecret("846B62D0-DEF9-4215-A99D-86E6B8DAB342");
+
+        // Register the System.Net.Http integration.
+        options.UseSystemNetHttp();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+builder.Services.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+//Ê¹ÓÃ¿çÓòÅäÖÃ
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api", [Authorize] (ClaimsPrincipal user) => $"{user.Identity!.Name} is allowed to access Api1.");
+
+app.Run();
